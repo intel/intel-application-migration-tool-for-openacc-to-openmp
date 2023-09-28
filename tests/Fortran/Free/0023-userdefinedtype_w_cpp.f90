3f5be1a1200e71c94d9549c310ff2c3235cc92a7
Harald Servat <harald.servat@intel.com>
2023-09-28 10:29:21 -0700

    Adding tests for CPP preprocessor in user defined types

---
+++ tests/Fortran/Free/0023-userdefinedtype_w_cpp.f90
@@ -0,0 +1,65 @@
+module m_test
+  implicit none
+  private
+  public t_date, t_hour, t_hour_ns
+
+  type :: t_date
+    integer :: year
+    integer :: month
+#if defined(A) && defined(B) && \
+    defined(C)
+    integer :: day
+#endif // A & B & C
+  end type
+
+  type :: t_hour
+    integer :: hour, &
+    minute
+#ifdef D
+    integer :: second
+#endif // D
+  end type
+
+  type, extends(t_hour) :: t_hour_ns
+    integer :: nanosecond
+  end type
+
+end module m_test
+
+program test
+  use m_test
+  implicit none
+  type(t_date) :: tdate
+  type(t_hour) :: thour1
+  type(t_hour_ns) :: thour2
+
+  tdate%year = 2022
+  tdate%month = 7
+#if defined(A) && defined(B) && defined(C)
+  tdate%day = 13
+#endif
+
+  !$acc serial copy(tdate)
+  tdate%month = tdate%month + 1
+  !$acc end serial
+
+  thour1%hour = 23
+  thour1%minute = 59
+#ifdef D
+  thour1%second = 58
+#endif
+
+  !$acc serial copyin(thour1)
+  thour1%hour = thour1%hour - 1
+  !$acc end serial
+
+  !$acc serial copyout(thour2)
+  thour2%hour = 23
+  thour2%minute = 59
+#if defined(D)
+  thour2%second = 58
+#endif
+  thour2%nanosecond = 100
+  !$acc end serial
+
+end program test
